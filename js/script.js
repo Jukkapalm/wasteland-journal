b63baf198f848f63ce1f118d38b00bce7c64786f
Juginhikipaja <jukka.palm@hotmail.fi>
2025-12-30 01:14:40 +0200

    Lisätty radioon viestejä

--- js/script.js
+++ js/script.js
@@ -20,5 +20,56 @@ function updatePaivat() {
     paivat.textContent = `🕒 ${paivatEro} päivää katastrofista`;
 }
 
-// Ladataan sivu ja käynnistetään funktio
-window.onload = updatePaivat();
+let vari = "#39FF14";
+
+function updateRadio() {
+    const radio = document.getElementById("radio-signal");
+
+    const viestit = [
+        { viesti: "Tuntematon signaali vastaanotettu" },
+        { viesti: "Jos joku lukee tämän, pysykää poissa kaupungista" },
+        { viesti: "Varoitus! Eteläinen sektori saastunut" },
+        { viesti: "Pysykää turvassa. Älkää jääkö yksin ulos" },
+        { viesti: "Älkää juoko käsittelemätöntä vettä, vedenpuhdistamot saastuneet" },
+        { viesti: "Ryöstäjiä liikkeellä, pysykää piilossa" },
+        { viesti: "Varoitus! Läntinen sektori saastunut" },
+        { viesti: "Kaupunki vihamielinen, välttäkää liikkumista keskustassa" },
+        { viesti: "Hälytys: Kaupunki täynnä väijyviä ryhmiä, liikkuminen vaarallista" },
+        { viesti: "Vältä liikkumista eteläisessä sektorissa, taloissa sortumavaara" },
+        { viesti: "Itäisellä sektorilla liikkuminen turvallista"},
+        { viesti: "Pohjoisella sektorilla turvallista liikkua" }
+    ];
+
+    const randomIndex = Math.floor(Math.random() * viestit.length);
+    const valittu = viestit[randomIndex];
+
+
+    if ([
+        "Tuntematon signaali vastaanotettu",
+        "Itäisellä sektorilla liikkuminen turvallista",
+        "Pohjoisella sektorilla turvallista liikkua"
+    ].includes(valittu.viesti)) {
+        vari = "#39FF14";
+    } else if ([
+        "Jos joku lukee tämän, pysykää poissa kaupungista",
+        "Pysykää turvassa. Älkää jääkö yksin ulos",
+        "Ryöstäjiä liikkeellä, pysykää piilossa",
+        "Vältä liikkumista eteläisessä sektorissa, taloissa sortumavaara"
+    ].includes(valittu.viesti)) {
+        vari = "#FFD700";
+    } else {
+        vari = "#FF3B3B";
+    }
+
+    radio.style.color = vari;
+    radio.textContent = `📻 ${valittu.viesti}`;
+
+}
+
+// Ladataan sivu ja käynnistetään funktiot
+window.onload = () => {
+    updatePaivat();
+    updateRadio();
+
+    setInterval(updateRadio, 10000);
+}
